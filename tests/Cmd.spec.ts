1006de15f2807974b7df3e0434e09f30dc1a7a96
Henrik Fuchs <atheck@gmail.com>
2021-04-08 13:43:23 +0200

    test: add missing tests

--- tests/Cmd.spec.ts
+++ tests/Cmd.spec.ts
@@ -80,13 +80,115 @@ describe("Cmd", () => {
                 expect(message).toBe("error");
             });
         });
+
+        describe("perform", () => {
+            it("returns one function", () => {
+                // arrange
+                const result = cmd.ofFunc.perform(jest.fn(), successMsg);
+
+                // assert
+                expect(result).toHaveLength(1);
+                expect(result[0]).toBeInstanceOf(Function);
+            });
+
+            it("runs the task", () => {
+                // arrange
+                const task = jest.fn();
+                const result = cmd.ofFunc.perform(task, successMsg);
+
+                // act
+                result[0](() => null);
+
+                // assert
+                expect(task).toHaveBeenCalledTimes(1);
+            });
+
+            it("dispatches the success message", () => {
+                // arrange
+                const task = jest.fn();
+                const result = cmd.ofFunc.perform(task, successMsg);
+
+                // act
+                let message = "";
+
+                result[0](msg => message = msg);
+
+                // assert
+                expect(message).toBe("success");
+            });
+
+            it("ignores an error", () => {
+                // arrange
+                const task = jest.fn(() => { throw new Error("error"); });
+                const result = cmd.ofFunc.perform(task, successMsg);
+
+                // act
+                let message = "";
+
+                result[0](msg => message = msg);
+
+                // assert
+                expect(message).toBe("");
+            });
+        });
+
+        describe("attempt", () => {
+            it("returns one function", () => {
+                // arrange
+                const result = cmd.ofFunc.attempt(jest.fn(), errorMsg);
+
+                // assert
+                expect(result).toHaveLength(1);
+                expect(result[0]).toBeInstanceOf(Function);
+            });
+
+            it("runs the task", () => {
+                // arrange
+                const task = jest.fn();
+                const result = cmd.ofFunc.attempt(task, errorMsg);
+
+                // act
+                result[0](() => null);
+
+                // assert
+                expect(task).toHaveBeenCalledTimes(1);
+            });
+
+            it("ignores the success", () => {
+                // arrange
+                const task = jest.fn();
+                const result = cmd.ofFunc.attempt(task, errorMsg);
+
+                // act
+                let message = "";
+
+                result[0](msg => message = msg);
+
+                // assert
+                expect(message).toBe("");
+            });
+
+            it("dispatches the error message", () => {
+                // arrange
+                const task = jest.fn(() => { throw new Error("error"); });
+                const result = cmd.ofFunc.attempt(task, errorMsg);
+
+                // act
+                let message = "";
+
+                result[0](msg => message = msg);
+
+                // assert
+                expect(message).toBe("error");
+            });
+        });
     });
 
     describe("ofPromise", () => {
         describe("either", () => {
             it("returns one function", () => {
                 // arrange
-                const result = cmd.ofPromise.either(resolveTask, () => "success", errorMsg);
+                const result = cmd.ofPromise.either(resolveTask, successMsg, errorMsg);
 
                 // assert
                 expect(result).toHaveLength(1);
@@ -96,7 +198,7 @@ describe("Cmd", () => {
             it("runs the task", () => {
                 // arrange
                 const task = jest.fn(resolveTask);
-                const result = cmd.ofPromise.either(task, () => "success", errorMsg);
+                const result = cmd.ofPromise.either(task, successMsg, errorMsg);
 
                 // act
                 result[0](() => null);
@@ -127,6 +229,112 @@ describe("Cmd", () => {
                 // arrange
                 const task = jest.fn(() => new Promise((_, reject) => reject("error")));
                 const result = cmd.ofPromise.either(task, successMsg, errorMsg);
+
+                // act
+                const act = (async () => {
+                    return new Promise(resolve => {
+                        result[0](resolve);
+                    });
+                });
+                const message = await act();
+
+                // assert
+                expect(message).toBe("error");
+                done();
+            });
+        });
+
+        describe("perform", () => {
+            it("returns one function", () => {
+                // arrange
+                const result = cmd.ofPromise.perform(resolveTask, successMsg);
+
+                // assert
+                expect(result).toHaveLength(1);
+                expect(result[0]).toBeInstanceOf(Function);
+            });
+
+            it("runs the task", () => {
+                // arrange
+                const task = jest.fn(resolveTask);
+                const result = cmd.ofPromise.perform(task, successMsg);
+
+                // act
+                result[0](() => null);
+
+                // assert
+                expect(task).toHaveBeenCalledTimes(1);
+            });
+
+            it("dispatches the success message", async done => {
+                // arrange
+                const task = jest.fn(resolveTask);
+                const result = cmd.ofPromise.perform(task, successMsg);
+
+                // act
+                const act = async () => {
+                    return new Promise(resolve => {
+                        result[0](resolve);
+                    });
+                };
+                const message = await act();
+
+                // assert
+                expect(message).toBe("success");
+                done();
+            });
+
+            it("ignores an error", async done => {
+                // arrange
+                const task = jest.fn(() => new Promise((_, reject) => reject("error")));
+                const result = cmd.ofPromise.perform(task, successMsg);
+
+                // act
+                result[0](jest.fn());
+
+                // assert
+                done();
+            });
+        });
+
+        describe("attempt", () => {
+            it("returns one function", () => {
+                // arrange
+                const result = cmd.ofPromise.attempt(resolveTask, errorMsg);
+
+                // assert
+                expect(result).toHaveLength(1);
+                expect(result[0]).toBeInstanceOf(Function);
+            });
+
+            it("runs the task", () => {
+                // arrange
+                const task = jest.fn(resolveTask);
+                const result = cmd.ofPromise.attempt(task, errorMsg);
+
+                // act
+                result[0](() => null);
+
+                // assert
+                expect(task).toHaveBeenCalledTimes(1);
+            });
+
+            it("ignores the success", async done => {
+                // arrange
+                const task = jest.fn(resolveTask);
+                const result = cmd.ofPromise.attempt(task, errorMsg);
+
+                // act
+                result[0](jest.fn());
+
+                // assert
+                done();
+            });
+
+            it("dispatches the error message", async done => {
+                // arrange
+                const task = jest.fn(() => new Promise((_, reject) => reject("error")));
+                const result = cmd.ofPromise.attempt(task, errorMsg);
 
                 // act
                 const act = (async () => {
