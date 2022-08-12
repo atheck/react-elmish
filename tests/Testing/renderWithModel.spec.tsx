79fbdb5a818a8484632fecd7935b6ba7a272ad0d
Henrik Fuchs <atheck@gmail.com>
2022-08-12 08:12:13 +0200

    test: add missing tests

---
+++ tests/Testing/renderWithModel.spec.tsx
@@ -0,0 +1,119 @@
+/* eslint-disable testing-library/prefer-user-event */
+import { ElmComponent, useElmish } from "../../src";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { InitResult, UpdateReturnType } from "../../src/Types";
+import { ReactNode } from "react";
+import { renderWithModel } from "../../src/Testing";
+
+type Message =
+    | { name: "click" };
+
+interface Model {
+    value: string,
+}
+
+interface Props {}
+
+function init (): InitResult<Model, Message> {
+    return [{ value: "" }];
+}
+
+function update (): UpdateReturnType<Model, Message> {
+    return [{}];
+}
+
+function TestComponent (): JSX.Element {
+    const [{ value }, dispatch] = useElmish({ name: "TestComponent", props: {}, init, update });
+
+    return (
+        <div>
+            <p>
+                {value}
+            </p>
+            <button
+                type={"button"}
+                onClick={() => dispatch({ name: "click" })}
+            >
+                {"Click"}
+            </button>
+        </div>
+    );
+}
+
+class TestClassComponent extends ElmComponent<Model, Message, Props> {
+    public constructor (props: Props) {
+        super(props, init, "TestClassComponent");
+    }
+
+    public update = update;
+
+    public override render (): ReactNode {
+        const { value } = this.model;
+
+        return (
+            <div>
+                <p>
+                    {value}
+                </p>
+                <button
+                    type={"button"}
+                    onClick={() => this.dispatch({ name: "click" })}
+                >
+                    {"Click"}
+                </button>
+            </div>
+        );
+    }
+}
+
+describe("renderWithModel", () => {
+    it("renders function component with provided model", () => {
+        // arrange
+        const model: Model = { value: "It works" };
+
+        // act
+        renderWithModel(() => render(<TestComponent />), model);
+
+        // assert
+        expect(screen.getByText("It works")).not.toBeNull();
+    });
+
+    it("uses the provided fake dispatch with a function component", async () => {
+        // arrange
+        const model: Model = { value: "" };
+        const mockDispatch = jest.fn();
+
+        renderWithModel(() => render(<TestComponent />), { model, dispatch: mockDispatch });
+
+        // act
+        fireEvent.click(screen.getByText("Click"));
+
+        // assert
+        expect(mockDispatch).toHaveBeenCalledWith({ name: "click" });
+    });
+
+    it("renders class component with provided model", () => {
+        // arrange
+        const model: Model = { value: "It works" };
+
+        // act
+        renderWithModel(() => render(<TestClassComponent />), model);
+
+        // assert
+        expect(screen.getByText("It works")).not.toBeNull();
+    });
+
+    it("uses the provided fake dispatch with a class component", async () => {
+        // arrange
+        const model: Model = { value: "" };
+        const mockDispatch = jest.fn();
+
+        renderWithModel(() => render(<TestClassComponent />), { model, dispatch: mockDispatch });
+
+        // act
+        fireEvent.click(screen.getByText("Click"));
+
+        // assert
+        expect(mockDispatch).toHaveBeenCalledWith({ name: "click" });
+    });
+});
