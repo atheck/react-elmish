596493643152fb59073b64f587d9ccb889c10bc6
Henrik Fuchs <atheck@gmail.com>
2022-08-19 07:50:57 +0200

    test: add missing tests

--- tests/ErrorHandling.spec.ts
+++ tests/ErrorHandling.spec.ts
@@ -1,8 +1,36 @@
 import { UpdateReturnType } from "../src";
-import { handleError } from "../src/ErrorHandling";
+import { errorHandler, errorMsg, handleError } from "../src/ErrorHandling";
 import { init, Logger } from "../src/Init";
 
 describe("ErrorHandling", () => {
+    describe("errorMsg", () => {
+        it("creates the correct message", () => {
+            // arrange
+            const error = new Error("Message");
+
+            // act
+            const result = errorMsg.error(error);
+
+            // assert
+            expect(result).toStrictEqual({ name: "error", error });
+        });
+    });
+
+    describe("errorHandler", () => {
+        it("returns object with an error handler function", () => {
+            // arrange
+
+            // act
+            const result = errorHandler();
+
+            result.error({ name: "error", error: new Error("Message") });
+
+            // assert
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            expect(result).toStrictEqual({ error: expect.any(Function) });
+        });
+    });
+
     describe("handleError", () => {
         it("returns empty new model", () => {
             // act
