d426d3c9be91374104aedb9f74b632dc9b0bfd68
Henrik Fuchs <atheck@gmail.com>
2022-10-14 09:45:52 +0200

    chore: remove install script as it is not working as intended

--- installScript.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-// eslint-disable-next-line no-console
-console.warn("react-elmish: Symbols for message names are deprecated now and will be removed in the next major version. Please convert all message names into strings.");
