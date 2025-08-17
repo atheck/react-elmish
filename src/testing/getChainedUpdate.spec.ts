9116a68d74e12c6b35b2ccc6abab21d524585f0f
Henrik Fuchs <atheck@gmail.com>
2025-08-17 16:11:02 +0200

    feat: add new test function `getChainedUpdate` for testing subsequent updates

---
+++ src/testing/getChainedUpdate.spec.ts
@@ -0,0 +1,109 @@
+import { cmd } from "../cmd";
+import type { InitResult, UpdateMap } from "../Types";
+import { getChainedUpdateFn } from "./getChainedUpdate";
+
+interface Model {
+	value1: string;
+	value2: string;
+	value3: string;
+}
+
+interface Props {}
+
+type Message =
+	| { name: "first" }
+	| { name: "second" }
+	| { name: "third" }
+	| { name: "firstWithDefer" }
+	| { name: "jumpToThird" }
+	| { name: "somethingAsync" };
+
+const Msg = {
+	first: (): Message => ({ name: "first" }),
+	second: (): Message => ({ name: "second" }),
+	third: (): Message => ({ name: "third" }),
+	firstWithDefer: (): Message => ({ name: "firstWithDefer" }),
+	jumpToThird: (): Message => ({ name: "jumpToThird" }),
+	somethingAsync: (): Message => ({ name: "somethingAsync" }),
+};
+
+function init(): InitResult<Model, Message> {
+	return [
+		{
+			value1: "",
+			value2: "",
+			value3: "",
+		},
+	];
+}
+
+const update: UpdateMap<Props, Model, Message> = {
+	first() {
+		return [{ value1: "1" }, cmd.ofMsg(Msg.second())];
+	},
+
+	second() {
+		return [{ value2: "2" }, cmd.ofMsg(Msg.third())];
+	},
+
+	third() {
+		return [{ value3: "3" }];
+	},
+
+	firstWithDefer(_msg, _model, _props, { defer }) {
+		defer({ value1: "1" });
+
+		return [{}, cmd.ofMsg(Msg.second())];
+	},
+
+	jumpToThird() {
+		return [{}, cmd.ofMsg(Msg.third())];
+	},
+
+	somethingAsync() {
+		return [{ value1: "async" }, cmd.ofSuccess(doSomething, Msg.third)];
+	},
+};
+
+async function doSomething(): Promise<void> {
+	// does nothing
+}
+
+describe("getChainedUpdate", () => {
+	const chainedUpdate = getChainedUpdateFn(update);
+
+	it("executes all messages and returns the correct end state", async () => {
+		// act
+		const model = await chainedUpdate(Msg.first(), init()[0], {});
+
+		// assert
+		expect(model).toStrictEqual<Model>({ value1: "1", value2: "2", value3: "3" });
+	});
+
+	it("executes all messages and returns the correct end state including deferred values", async () => {
+		// act
+		const model = await chainedUpdate(Msg.firstWithDefer(), init()[0], {});
+
+		// assert
+		expect(model).toStrictEqual<Model>({ value1: "1", value2: "2", value3: "3" });
+	});
+
+	it("only returns the partial updated state", async () => {
+		// arrange
+		const initialModel: Model = { value1: "0", value2: "0", value3: "0" };
+
+		// act
+		const model = await chainedUpdate(Msg.jumpToThird(), initialModel, {});
+
+		// assert
+		expect(model).toStrictEqual<Partial<Model>>({ value3: "3" });
+	});
+
+	it("works with async operations", async () => {
+		// act
+		const model = await chainedUpdate(Msg.somethingAsync(), init()[0], {});
+
+		// assert
+		expect(model).toStrictEqual<Partial<Model>>({ value1: "async", value3: "3" });
+	});
+});
---
+++ src/testing/getChainedUpdate.ts
@@ -0,0 +1,39 @@
+import type { Message, Nullable, UpdateFunctionOptions, UpdateMap } from "../Types";
+import { getUpdateAndExecCmdFn } from "./getUpdateFn";
+
+function getChainedUpdateFn<TProps, TModel, TMessage extends Message>(
+	updateMap: UpdateMap<TProps, TModel, TMessage>,
+): (
+	msg: TMessage,
+	model: TModel,
+	props: TProps,
+	optionsTemplate?: Partial<UpdateFunctionOptions<TProps, TModel, TMessage>>,
+) => Promise<Partial<TModel>> {
+	return async function chainedUpdateFn(msg, model, props, optionsTemplate): Promise<Partial<TModel>> {
+		let messages: TMessage[] = [msg];
+		let currentModel = model;
+		let totalUpdatedModel: Partial<TModel> = {};
+
+		const updatedAndExecFn = getUpdateAndExecCmdFn(updateMap);
+
+		while (messages.length > 0) {
+			const currentMessages: Nullable<TMessage>[] = [];
+
+			for (const nextMsg of messages) {
+				// biome-ignore lint/nursery/noAwaitInLoop: We need to await each update sequentially
+				const [updatedModel, newMessages] = await updatedAndExecFn(nextMsg, currentModel, props, optionsTemplate);
+
+				currentModel = { ...currentModel, ...updatedModel };
+				totalUpdatedModel = { ...totalUpdatedModel, ...updatedModel };
+
+				currentMessages.push(...newMessages);
+			}
+
+			messages = currentMessages.filter((message) => message != null);
+		}
+
+		return totalUpdatedModel;
+	};
+}
+
+export { getChainedUpdateFn };
