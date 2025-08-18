21432c5e7177d65d59fb88e0a15497eaaaaab756
Henrik Fuchs <atheck@gmail.com>
2025-08-18 08:38:39 +0200

    fix: export `getConsecutiveUpdateFn`

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -3,6 +3,7 @@ export { createModelAndProps } from "./createModelAndProps";
 export { createUpdateArgsFactory, type UpdateArgsFactory } from "./createUpdateArgsFactory";
 export { execCmd, execCmdWithDispatch } from "./execCmd";
 export { execSubscription } from "./execSubscription";
+export { getConsecutiveUpdateFn } from "./getConsecutiveUpdate";
 export { getCreateModelAndProps, getCreateUpdateArgs, type ModelAndPropsFactory } from "./getCreateUpdateArgs";
 export { getUpdateAndExecCmdFn, getUpdateFn } from "./getUpdateFn";
 export { initAndExecCmd } from "./initAndExecCmd";
