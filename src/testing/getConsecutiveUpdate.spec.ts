a45aa2ee5b208dcdd8e56902d5442320dde3f7a7
Henrik Fuchs <atheck@gmail.com>
2025-08-18 17:47:02 +0200

    test: add tests for `defer` and  `callBase`

--- src/testing/getConsecutiveUpdate.spec.ts
+++ src/testing/getConsecutiveUpdate.spec.ts
@@ -1,11 +1,12 @@
 import { cmd } from "../cmd";
-import type { InitResult, UpdateMap } from "../Types";
+import type { InitResult, UpdateMap, UpdateReturnType } from "../Types";
 import { getConsecutiveUpdateFn } from "./getConsecutiveUpdate";
 
 interface Model {
 	value1: string;
 	value2: string;
 	value3: string;
+	data: number;
 }
 
 interface Props {}
@@ -16,7 +17,9 @@ type Message =
 	| { name: "third" }
 	| { name: "firstWithDefer" }
 	| { name: "jumpToThird" }
-	| { name: "somethingAsync" };
+	| { name: "somethingAsync" }
+	| { name: "somethingAsyncSuccess"; data: number }
+	| { name: "callBase" };
 
 const Msg = {
 	first: (): Message => ({ name: "first" }),
@@ -25,6 +28,8 @@ const Msg = {
 	firstWithDefer: (): Message => ({ name: "firstWithDefer" }),
 	jumpToThird: (): Message => ({ name: "jumpToThird" }),
 	somethingAsync: (): Message => ({ name: "somethingAsync" }),
+	somethingAsyncSuccess: (data: number): Message => ({ name: "somethingAsyncSuccess", data }),
+	callBase: (): Message => ({ name: "callBase" }),
 };
 
 function init(): InitResult<Model, Message> {
@@ -33,6 +38,7 @@ function init(): InitResult<Model, Message> {
 			value1: "",
 			value2: "",
 			value3: "",
+			data: 0,
 		},
 	];
 }
@@ -61,12 +67,26 @@ const update: UpdateMap<Props, Model, Message> = {
 	},
 
 	somethingAsync() {
-		return [{ value1: "async" }, cmd.ofSuccess(doSomething, Msg.third)];
+		return [{ value1: "async" }, cmd.ofSuccess(doSomething, Msg.somethingAsyncSuccess)];
+	},
+
+	somethingAsyncSuccess({ data }) {
+		return [{ data }];
+	},
+
+	callBase(_msg, _model, _props, { callBase, defer }) {
+		defer(...callBase(base));
+
+		return [{}];
 	},
 };
 
-async function doSomething(): Promise<void> {
-	// does nothing
+function base(): UpdateReturnType<Model, Message> {
+	return [{ value1: "base" }];
+}
+
+async function doSomething(): Promise<number> {
+	return 1;
 }
 
 describe("getConsecutiveUpdate", () => {
@@ -77,7 +97,7 @@ describe("getConsecutiveUpdate", () => {
 		const model = await consecutiveUpdate(Msg.first(), init()[0], {});
 
 		// assert
-		expect(model).toStrictEqual<Model>({ value1: "1", value2: "2", value3: "3" });
+		expect(model).toStrictEqual<Partial<Model>>({ value1: "1", value2: "2", value3: "3" });
 	});
 
 	it("executes all messages and returns the correct end state including deferred values", async () => {
@@ -85,12 +105,20 @@ describe("getConsecutiveUpdate", () => {
 		const model = await consecutiveUpdate(Msg.firstWithDefer(), init()[0], {});
 
 		// assert
-		expect(model).toStrictEqual<Model>({ value1: "1", value2: "2", value3: "3" });
+		expect(model).toStrictEqual<Partial<Model>>({ value1: "1", value2: "2", value3: "3" });
+	});
+
+	it("executes the base call and defer", async () => {
+		// act
+		const model = await consecutiveUpdate(Msg.callBase(), init()[0], {});
+
+		// assert
+		expect(model).toStrictEqual<Partial<Model>>({ value1: "base" });
 	});
 
 	it("only returns the partial updated state", async () => {
 		// arrange
-		const initialModel: Model = { value1: "0", value2: "0", value3: "0" };
+		const initialModel: Model = { value1: "0", value2: "0", value3: "0", data: 0 };
 
 		// act
 		const model = await consecutiveUpdate(Msg.jumpToThird(), initialModel, {});
@@ -104,6 +132,6 @@ describe("getConsecutiveUpdate", () => {
 		const model = await consecutiveUpdate(Msg.somethingAsync(), init()[0], {});
 
 		// assert
-		expect(model).toStrictEqual<Partial<Model>>({ value1: "async", value3: "3" });
+		expect(model).toStrictEqual<Partial<Model>>({ value1: "async", data: 1 });
 	});
 });
