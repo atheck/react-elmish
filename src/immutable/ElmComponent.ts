8690b1bac67faffce4ad9162b3bfbb7ede8ae8ef
Henrik Fuchs <atheck@gmail.com>
2025-09-28 13:57:09 +0200

    fix: update deps only to fix security issues

--- src/immutable/ElmComponent.ts
+++ src/immutable/ElmComponent.ts
@@ -25,7 +25,6 @@ abstract class ElmComponent<TModel, TMessage extends Message, TProps> extends Re
 	private readonly buffer: TMessage[] = [];
 	private running = false;
 	private mounted = false;
-	// biome-ignore lint/style/useReadonlyClassProperties: This is a false positive
 	private currentModel: Immutable<TModel>;
 
 	/**
